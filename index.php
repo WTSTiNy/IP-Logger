0ceff6e128d32b688b45b821ab7e25cf536ddbe3
TiNy <87048262+WTSTiNy@users.noreply.github.com>
2022-04-19 20:39:32 -0700

    I did not create it, github is lying...

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$logged = fopen("logged.txt", "a") or die("\[-] Unable to open \"logged.txt\"");
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+{
+  $ip = $_SERVER['HTTP_CLIENT_IP'];
+}
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+{
+  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+else
+{
+  $ip = $_SERVER['REMOTE_ADDR'];
+}
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+fwrite($logged, "IP Address: ".$ip."\n");
+fwrite($logged, "User-Agent: ".$user_agent."\n\n");
+fwrite($logged, "-------------------------------------------\n\n");
+?>
+
+# Gets User Agent String and IP Address, pretty simple
